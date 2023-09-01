f086f5204ff59407c8aafbb2bffcf2fc4a4637b8
小新 <1066321456@qq.com>
2023-09-01 11:20:23 +0800

    style: update file

--- arithmetic/2.冒泡算法/1.冒泡算法.c
+++ arithmetic/2.冒泡算法/1.冒泡算法.c
@@ -7,15 +7,15 @@
 	data[]，排好序的数组
 返回值：无 
 */
-void BubbleSort( int data[], int n )
+void BubbleSort(int data[], int n)
 {
 	int i, j, t;
-	for( i = 1; i <= n-1; i++ ) {
-		for( j = 1; j <= n-i; j++ ) {
-			if( data[j+1] < data[j] ) {
-				t         = data[j+1];
+	for ( i = 1; i <= n-1; i++) {
+		for ( j = 1; j <= n-i; j++) {
+			if (data[j+1] < data[j]) {
+				t = data[j+1];
 				data[j+1] = data[j];
-				data[j]   = t;
+				data[j] = t;
 			}
 		}
 	}
