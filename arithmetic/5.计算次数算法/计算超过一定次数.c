6ebbbf742d8dd0ed560b7a25adf5b2c3c008afcb
小新 <1066321456@qq.com>
2023-09-09 19:32:56 +0800

    chore: test file

--- arithmetic/5.计算次数算法/计算超过一定次数.c
+++ arithmetic/5.计算次数算法/计算超过一定次数.c
@@ -1,3 +1,10 @@
+/**
+ * @brief 这种思想大致就是谁的次数多，谁就能一直保存下去，
+ * 
+ * @param nums 
+ * @param numsSize 
+ * @return int 
+ */
 int majorityElement(int* nums, int numsSize){
     //时间复杂度为 O(n)、空间复杂度为 O(1)
     int sum =1,res = nums[0];
