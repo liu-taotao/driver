599950f1bc52060f41c7727262de3f36bf004f84
小新 <1066321456@qq.com>
2023-09-10 09:02:57 +0800

    style: test file

--- arithmetic/5.计算次数算法/计算超过一定次数.c
+++ arithmetic/5.计算次数算法/计算超过一定次数.c
@@ -5,15 +5,16 @@
  * @param numsSize 
  * @return int 
  */
-int majorityElement(int* nums, int numsSize){
+int majorityElement(int* nums, int numsSize)
+{
     //时间复杂度为 O(n)、空间复杂度为 O(1)
-    int sum =1,res = nums[0];
-    for(int i=1;i<numsSize;i++){
-        if(nums[i]==res)
+    int sum =1, res = nums[0];
+    for(int i=1; i<numsSize; i++){
+        if (nums[i]==res) 
             sum++;
         else
             sum--;
-        if(sum<0){
+        if (sum<0) {
             res=nums[i];
             sum=1;
         }
