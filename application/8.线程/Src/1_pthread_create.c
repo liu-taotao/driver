633874cb03cd13bae70d8b5aa35e88d982969f70
小新 <1066321456@qq.com>
2023-10-11 20:27:24 +0800

    fix: pthread_create()

---
+++ application/8.线程/Src/1_pthread_create.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+static void *new_thread_start(void *arg)
+{
+    printf("新线程: 进程ID<%d> 线程ID<%lu>\n", getpid(), pthread_self());
+    return (void*)0;
+}
+
+int main(void)
+{
+    pthread_t tid;
+    int ret;
+
+    ret = pthread_create(&tid, NULL, new_thread_start, NULL);
+    if (ret) {
+        fprintf(stderr, "error: %s\n", strerror(ret));
+        exit(-1);
+    }
+
+    printf("主线程: 进程ID<%d> 线程ID<%lu>\n", getpid(), pthread_self());
+    sleep(1);
+    exit(0);
+}
