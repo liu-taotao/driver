0a92a93f49094612d71606c2a0665fc4f4f1f750
小新 <1066321456@qq.com>
2023-10-15 10:03:01 +0800

    fix: update file

--- arithmetic/63.三角形最小路径和-动态规划/动态规划.c
+++ arithmetic/63.三角形最小路径和-动态规划/动态规划.c
@@ -1,5 +1,4 @@
 
-#define min(a,b)(a<b?a:b)
 
 /**
  * @brief 动态规划，算出所有路径的和，最后得出最后一行的最小值就是这个三角形的最小路径。
@@ -9,21 +8,22 @@
  * @param triangleColSize 
  * @return int 
  */
+
 int minimumTotal(int** triangle, int triangleSize, int* triangleColSize){
-    int dp[triangleSize][triangleSize];
-    memset(dp, 0, sizeof(dp));
-    dp[0][0] = triangle[0][0];
+    int res[triangleSize][triangleSize];
+    memset(res, 0, sizeof(int));
+    res[0][0] = triangle[0][0];
 
     for (int i = 1; i < triangleSize; i++) {
-        dp[i][0] = dp[i - 1][0]+triangle[i][0];
+        res[i][0] = res[i - 1][0] + triangle[i][0];
         for (int j = 1; j < i; j++) {
-            dp[i][j] = min(dp[i-1][j-1],dp[i-1][j]) + triangle[i][j];
+            res[i][j] = fmin(res[i - 1][j - 1], res[i - 1][j]) + triangle[i][j];
         }
-        dp[i][i]=dp[i - 1][i - 1] + triangle[i][i];
+        res[i][i] = res[i - 1][i - 1] + triangle[i][i];
     }
-    int ans = dp[triangleSize - 1][0];
+    int ans = res[triangleSize - 1][0];
     for (int i = 1; i < triangleSize; i++) {
-        ans = min(ans, dp[triangleSize - 1][i]);
+        ans = fmin(ans, res[triangleSize - 1][i]);
     }
     return ans;
 }
