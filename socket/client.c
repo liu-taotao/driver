17998e070cd0c832a75598eda0fe9d49834f734a
小新 <1066321456@qq.com>
2024-12-20 22:15:07 +0800

    fix: 更改server和client设置

--- socket/client.c
+++ socket/client.c
@@ -1,102 +1,57 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <arpa/inet.h>
 #include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#define SERVER_PORT 8888 //服务器的端口号
+#define SERVER_IP "192.168.1.25" //服务器的 IP 地址
 
-#define SERVER_PORT 12345
-#define BUFFER_SIZE 1024
-
-void connect_to_server(int *sockfd, struct sockaddr_in *server_addr);
-void get_current_time(int sockfd);
-void get_machine_name(int sockfd);
-void get_active_connections(int sockfd);
-void send_message(int sockfd);
-void disconnect(int sockfd);
-
-int main() {
+int main(void)
+{
+    struct sockaddr_in server_addr = {0};
+    char buf[512];
     int sockfd;
-    struct sockaddr_in server_addr;
-    int choice;
-
-    connect_to_server(&sockfd, &server_addr);
-
-    while (1) {
-        printf("\n1. 获取时间\n");
-        printf("2. 获取机器名称\n");
-        printf("3. 活动连接列表\n");
-        printf("4. 发消息\n");
-        printf("5. 退出\n");
-        printf("选择功能: ");
-        scanf("%d", &choice);
-
-        switch (choice) {
-            case 1: get_current_time(sockfd); break;
-            case 2: get_machine_name(sockfd); break;
-            case 3: get_active_connections(sockfd); break;
-            case 4: send_message(sockfd); break;
-            case 5: disconnect(sockfd); exit(0);
-            default: printf("无效选择\n");
-        }
+    int ret;
+    /* 打开套接字，得到套接字描述符 */
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (0 > sockfd) {
+        perror("socket error");
+        exit(EXIT_FAILURE);
     }
-
-    return 0;
-}
-
-void connect_to_server(int *sockfd, struct sockaddr_in *server_addr) {
-    *sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    server_addr->sin_family = AF_INET;
-    server_addr->sin_port = htons(SERVER_PORT);
-    inet_pton(AF_INET, "127.0.0.1", &server_addr->sin_addr);
-    connect(*sockfd, (struct sockaddr *)server_addr, sizeof(*server_addr));
-}
-
-void disconnect(int sockfd) {
+    /* 调用 connect 连接远端服务器 */
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(SERVER_PORT); //端口号
+    inet_pton(AF_INET, SERVER_IP, &server_addr.sin_addr);//IP 地址
+    ret = connect(sockfd, (struct sockaddr *)&server_addr, sizeof(server_addr));
+    if (0 > ret) {
+        perror("connect error");
+        close(sockfd);
+        exit(EXIT_FAILURE);
+    }
+    printf("服务器连接成功...\n\n");
+    /* 向服务器发送数据 */
+    for ( ; ; ) {
+        // 清理缓冲区
+        memset(buf, 0x0, sizeof(buf));
+        // 接收用户输入的字符串数据
+        printf("Please enter a string: ");
+        fgets(buf, sizeof(buf), stdin);
+        // 将用户输入的数据发送给服务器
+        ret = send(sockfd, buf, strlen(buf), 0);
+        if(0 > ret){
+            perror("send error");
+            break;
+        }
+        memset(buf, 0x0, sizeof(buf));
+        recv(sockfd, buf, sizeof(buf), 0);
+        printf("Server response: %s\n", buf);
+        //输入了"exit"，退出循环
+        if(0 == strncmp(buf, "exit", 4))
+            break;
+    }
     close(sockfd);
-}
-
-void get_current_time(int sockfd) {
-    unsigned char request[2] = {0x03, 0}; // 0x03: 获取时间
-    send(sockfd, request, sizeof(request), 0);
-    
-    char buffer[BUFFER_SIZE];
-    recv(sockfd, buffer, sizeof(buffer), 0);
-    printf("当前时间: %s\n", buffer);
-}
-
-void get_machine_name(int sockfd) {
-    unsigned char request[2] = {0x04, 0}; // 0x04: 获取机器名称
-    send(sockfd, request, sizeof(request), 0);
-
-    char buffer[BUFFER_SIZE];
-    recv(sockfd, buffer, sizeof(buffer), 0);
-    printf("机器名称: %s\n", buffer);
-}
-
-void get_active_connections(int sockfd) {
-    unsigned char request[2] = {0x05, 0}; // 0x05: 获取活动连接列表
-    send(sockfd, request, sizeof(request), 0);
-
-    char buffer[BUFFER_SIZE];
-    recv(sockfd, buffer, sizeof(buffer), 0);
-    printf("活动连接: %s\n", buffer);
-}
-
-void send_message(int sockfd) {
-    int target_id;
-    char message[BUFFER_SIZE];
-
-    printf("输入目标客户端编号: ");
-    scanf("%d", &target_id);
-    printf("输入消息: ");
-    getchar(); // consume newline
-    fgets(message, sizeof(message), stdin);
-    
-    // 构建发送消息请求
-    unsigned char request[BUFFER_SIZE];
-    request[0] = 0x06; // 0x06: 发消息
-    request[1] = target_id + '0'; // 目标编号
-    strcpy((char *)(request + 2), message);
-    
-    send(sockfd, request, strlen(message) + 2, 0);
+    exit(EXIT_SUCCESS);
 }
--- socket/server.c
+++ socket/server.c
@@ -1,115 +1,111 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
 #include <arpa/inet.h>
 #include <pthread.h>
 #include <time.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-
-#define PORT 12345
-#define MAX_CLIENTS 10
-#define BUFFER_SIZE 1024
-
-typedef struct {
-    int sockfd;
-    struct sockaddr_in addr;
-} client_info;
-
-client_info clients[MAX_CLIENTS];
-int client_count = 0;
-pthread_mutex_t clients_mutex = PTHREAD_MUTEX_INITIALIZER;
-
-void *handle_client(void *arg);
-void send_current_time(int client_sock);
-void send_machine_name(int client_sock);
-void send_active_connections(int client_sock);
-void forward_message(int sender_sock, const char *message, int target_id);
-
-int main() {
-    int server_sock, client_sock;
-    struct sockaddr_in server_addr, client_addr;
-    socklen_t addr_len = sizeof(client_addr);
-    pthread_t thread_id;
-
-    server_sock = socket(AF_INET, SOCK_STREAM, 0);
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_addr.s_addr = INADDR_ANY;
-    server_addr.sin_port = htons(PORT);
-    bind(server_sock, (struct sockaddr *)&server_addr, sizeof(server_addr));
-    listen(server_sock, MAX_CLIENTS);
-
-    printf("Server is listening on port %d\n", PORT);
-
-    while ((client_sock = accept(server_sock, (struct sockaddr *)&client_addr, &addr_len))) {
-        pthread_mutex_lock(&clients_mutex);
-        if (client_count < MAX_CLIENTS) {
-            clients[client_count].sockfd = client_sock;
-            clients[client_count].addr = client_addr;
-            client_count++;
-            pthread_create(&thread_id, NULL, handle_client, (void *)&clients[client_count - 1]);
-        } else {
-            printf("Max clients connected. Rejecting connection.\n");
-            close(client_sock);
-        }
-        pthread_mutex_unlock(&clients_mutex);
+#include <sys/utsname.h>
+#define SERVER_PORT 8888 //端口号不能发生冲突,不常用的端口号通常大于 5000
+// 在 recv 之后立即调用此函数
+char* trim_newline(char* str) {
+    size_t len = strlen(str);
+    if (len > 0 && str[len - 1] == '\n') {
+        str[len - 1] = '\0';
     }
-
-    close(server_sock);
-    return 0;
+    return str;
 }
-
 void *handle_client(void *arg) {
-    client_info *client = (client_info *)arg;
-    int client_sock = client->sockfd;
-    char buffer[BUFFER_SIZE];
-    int bytes_read;
-
-    while ((bytes_read = recv(client_sock, buffer, sizeof(buffer), 0)) > 0) {
-        buffer[bytes_read] = '\0'; // Null-terminate the string
-
-        if (strcmp(buffer, "TIME") == 0) {
-            send_current_time(client_sock);
-        } else if (strcmp(buffer, "NAME") == 0) {
-            send_machine_name(client_sock);
-        } else if (strcmp(buffer, "ACTIVE") == 0) {
-            send_active_connections(client_sock);
-        } else if (strncmp(buffer, "MSG ", 4) == 0) {
-            int target_id = buffer[4] - '0'; // Extract the target ID
-            forward_message(client_sock, buffer + 6, target_id); // Forward message
+    int client_socket = *((int *)arg);
+    int ans = 0;
+    char buffer[1024] = {0};
+    
+    while (1) {
+        memset(buffer, 0, sizeof(buffer));
+        ans = recv(client_socket, buffer, sizeof(buffer), 0);
+        // 将读取到的数据以字符串形式打印出来   
+        printf("from client: %s\n", buffer);
+        trim_newline(buffer); // 去除换行符
+        if (strcmp(buffer, "time") == 0) {
+            time_t rawtime;
+            struct tm *timeinfo;
+            time(&rawtime);
+            timeinfo = localtime(&rawtime);
+            char *time_str = asctime(timeinfo);
+            printf("send client: %s\n", time_str);
+            send(client_socket, time_str, strlen(time_str), 0);
+        } 
+        else if (strcmp(buffer, "hostname") == 0) {
+            struct utsname info;
+            uname(&info);
+            send(client_socket, info.nodename, strlen(info.nodename), 0);
+        }
+        // 如果读取到"exit"则关闭套接字退出程序
+        if (0 == strncmp("exit", buffer, 4)) {
+            printf("server exit...\n");
+            printf("Client disconnected: %d\n", client_socket);
+            send(client_socket, buffer, strlen(buffer), 0);
+            //close(client_socket);
+            break;
+        }
+        if (ans <= 0 ) {
+            printf("Client disconnected: %d\n", client_socket);
+            break; // 退出循环，结束线程
         }
     }
-
-    close(client_sock);
-    pthread_mutex_lock(&clients_mutex);
-    client_count--;
-    pthread_mutex_unlock(&clients_mutex);
-    return NULL;
+    
+    close(client_socket);
+    pthread_exit(NULL);
 }
-
-void send_current_time(int client_sock) {
-    time_t now = time(NULL);
-    char *time_str = ctime(&now);
-    send(client_sock, time_str, strlen(time_str), 0);
-}
-
-void send_machine_name(int client_sock) {
-    char hostname[BUFFER_SIZE];
-    gethostname(hostname, sizeof(hostname));
-    send(client_sock, hostname, strlen(hostname), 0);
-}
-
-void send_active_connections(int client_sock) {
-    char buffer[BUFFER_SIZE];
-    int len = snprintf(buffer, sizeof(buffer), "Active clients: %d\n", client_count);
-    send(client_sock, buffer, len, 0);
-}
-
-void forward_message(int sender_sock, const char *message, int target_id) {
-    pthread_mutex_lock(&clients_mutex);
-    if (target_id >= 0 && target_id < client_count) {
-        send(clients[target_id].sockfd, message, strlen(message), 0);
+int main(void)
+{
+    struct sockaddr_in server_addr = {0};
+    struct sockaddr_in client_addr = {0};
+    char ip_str[20] = {0};
+    int sockfd, connfd;
+    int addrlen = sizeof(client_addr);
+    char recvbuf[512];
+    int ret;
+    /* 打开套接字，得到套接字描述符 */
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (0 > sockfd) {
+        perror("socket error");
+        exit(EXIT_FAILURE);
     }
-    pthread_mutex_unlock(&clients_mutex);
+    /* 将套接字与指定端口号进行绑定 */
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    server_addr.sin_port = htons(SERVER_PORT);
+    ret = bind(sockfd, (struct sockaddr *)&server_addr, sizeof(server_addr));
+    if (0 > ret) {
+        perror("bind error");
+        close(sockfd);
+        exit(EXIT_FAILURE);
+    }
+    /* 使服务器进入监听状态 */
+    ret = listen(sockfd, 50);
+    if (0 > ret) {
+        perror("listen error");
+        close(sockfd);
+        exit(EXIT_FAILURE);
+    }
+
+    pthread_t tid;
+    
+    while (1) {
+        connfd = accept(sockfd, (struct sockaddr *)&client_addr, &addrlen);
+        printf("有客户端接入...\n");
+        inet_ntop(AF_INET, &client_addr.sin_addr.s_addr, ip_str, sizeof(ip_str));
+        printf("客户端主机的 IP 地址: %s\n", ip_str);
+        printf("客户端进程的端口号: %d\n", client_addr.sin_port);
+        pthread_create(&tid, NULL, handle_client, (void *)&connfd);
+        pthread_detach(tid);
+    }
+    /* 关闭套接字 */
+    close(sockfd);
+    exit(EXIT_SUCCESS);
 }
